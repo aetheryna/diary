9da23e7d58c7c1a2ac93cc60ae63301096f726de
AetheryNA <naveed@nimblehq.co>
2022-11-08 15:27:57 +0700

    [001] Header component

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className='header'>
+      <div className='container'>
+        <h3>Diary</h3>
+      </div>
+    </header>
+  )
+}
+
+export default Header
